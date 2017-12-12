219f29681e38faa62cb7e00941acbc0166593f88
cadtuxjack <cadtuxjack@gmail.com>
2017-12-12 10:18:33 +0100

    Proyecto inicial con Bootstrap

--- p01-base/src/app/app.component.ts
+++ p01-base/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html', //vista de componente
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
